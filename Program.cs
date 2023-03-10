0e7e0bf54545f8f01079c1acd60eceffe9e5b677
dev-xpeace <xpeacedev@outlook.com>
2023-03-10 03:37:13 +0300

    basic-calculator
    a basic calculator developed with C#

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter Your Name: ");
+string name = Console.ReadLine();
+Console.Write("Enter Your Age: ");
+string age = Console.ReadLine();
+
+Console.Write("Hello! " + name + " and you are " + age + " nice to meet you " + name);
+Console.ReadLine();
